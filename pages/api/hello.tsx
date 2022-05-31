7873b541bafdfc4d764d30b0e63b0d2c5e22e3dd
pleskac <markpleskac@gmail.com>
2022-05-31 13:52:38 -0700

    chore: fix build

--- pages/api/hello.tsx
+++ pages/api/hello.tsx
@@ -1,8 +1,7 @@
-
 import { NextApiRequest, NextApiResponse } from "next";
 
-const handler = (req: NextApiRequest, res: NextApiResponse) {
-  res.status(200).json({ status: 'Not implemented.... yet?' })
-}
+const handler = (req: NextApiRequest, res: NextApiResponse) => {
+    res.status(200).json({ status: "Not implemented.... yet?" });
+};
 
 export default handler;
