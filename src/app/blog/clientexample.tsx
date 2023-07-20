714414c2d11e370503f88032896942a6388ef12d
pleskac <markpleskac@gmail.com>
2023-07-20 11:32:11 -0700

    push

--- src/app/blog/clientexample.tsx
+++ src/app/blog/clientexample.tsx
@@ -1,5 +1,14 @@
 "use client";
+import { css } from "../../../styled-system/css";
 
 export function ClientExample() {
-    return <span>I am on the client!</span>;
+    return (
+        <span
+            className={css({
+                fontWeight: "700",
+            })}
+        >
+            I am on the client!
+        </span>
+    );
 }
